b1627d50a32ce646e7a7ea549f947f774293f931
Santiago Loaiza <santiagoloaizamadrid@gmail.com>
2025-03-18 14:55:57 -0500

    MainContent Modified

---
+++ src/MainContent.jsx
@@ -0,0 +1,9 @@
+import './MainContent.css'
+const MainContent = () => {
+    return (
+        <section className="main-content">
+            <div className='carrusel'><h1>Carrusel</h1></div>
+        </section>
+    )
+}
+export default MainContent
