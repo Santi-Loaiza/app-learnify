95bbb4fd3c6fdb7159d5e516fbe1af1230a1b05a
Santiago Loaiza <santiagoloaizamadrid@gmail.com>
2025-03-19 21:05:12 -0500

    Improved component layout and change of footer layout

--- src/MainView.jsx
+++ src/MainView.jsx
@@ -1,3 +1,4 @@
+import Footer from "./Footer"
 import MainContent from "./MainContent"
 import SideMenu from "./SideMenu"
 
@@ -6,8 +7,10 @@ const MainView = () => {
     return(
         <section className="main-container">
             <SideMenu />
-            <MainContent />
-            <Footer />
+            <section>
+                 <MainContent />
+                 <Footer />
+            </section>
         </section>
     )
 }
